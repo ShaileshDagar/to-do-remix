9f89c9a0949bd283dd8348efade5e3ec8662739f
Shailesh Dagar <52007469+ShaileshDagar@users.noreply.github.com>
2024-01-23 14:15:08 +0530

    Add defer-await-suspense to list page

--- app/routes/list.tsx
+++ app/routes/list.tsx
@@ -1,5 +1,6 @@
-import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node"
-import { Form, useLoaderData } from "@remix-run/react"
+import { ActionFunctionArgs, LoaderFunctionArgs, defer } from "@remix-run/node"
+import { Await, Form, useLoaderData } from "@remix-run/react"
+import { Suspense } from "react"
 import { redirect } from "react-router"
 import { client, createTask, deleteTask, getList } from "~/pocketbase"
 
@@ -9,15 +10,13 @@ export async function loader({request}: LoaderFunctionArgs) {
         const message = "You must login first!"
         throw redirect(`/login?message=${message}`)
     }
-    // console.log(
-    return getList()
+    return defer({list: getList()})
     
 }
 
 export async function action({request}: ActionFunctionArgs) {
     const formData = await request.formData()
     const _action = formData.get("_action")
-    console.log(formData)
     if(_action === "create"){
         const task = formData.get("task")
         await createTask(task, client.authStore.model?.id)
@@ -30,21 +29,29 @@ export async function action({request}: ActionFunctionArgs) {
 }
 
 export default function List() {
-    //
-    const list = useLoaderData<typeof loader>()
-    const listEls = list.items.map((item) => {
-        return (
-            <li key={item.id}>{item.task}{" "}
-                <Form method="DELETE" style={{display: "inline"}}>
-                    <input type="hidden" name="id" value={item.id}/>
-                    <button type="submit" aria-label="delete" name="_action" value="delete">Delete</button>
-                </Form>
-            </li>)
-    })
+    const loaderDataPromise = useLoaderData<typeof loader>()
+    
+    function renderList(loadedList: { items: any[] }) {
+        const listEls = loadedList.items.map((item) => {
+            return (
+                <li key={item.id}>{item.task}{" "}
+                    <Form method="DELETE" style={{display: "inline"}}>
+                        <input type="hidden" name="id" value={item.id}/>
+                        <button type="submit" aria-label="delete" name="_action" value="delete">Delete</button>
+                    </Form>
+                </li>)
+        })
+        return listEls
+    }
+    
     return <div>
         <h2>Pending Tasks:</h2>
         <ul>
-            {listEls}
+            <Suspense fallback={<h1>Loading List...</h1>}>
+                <Await resolve={loaderDataPromise.list}>
+                    {renderList}
+                </Await>
+            </Suspense>
             <li>
                 <Form method="POST">
                     <input 
